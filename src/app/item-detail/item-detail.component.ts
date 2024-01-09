02735bcb3f76b10e9dd6ecf5d8052a4d664ddb7d
Clinton Mutinda <cmutinda@mfs.co.ke>
2024-01-09 12:41:17 +0300

    Configured the child component

---
+++ src/app/item-detail/item-detail.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-item-detail',
+  templateUrl: './item-detail.component.html',
+  styleUrls: ['./item-detail.component.scss']
+})
+export class ItemDetailComponent implements OnInit {
+
+  @Input() item = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
